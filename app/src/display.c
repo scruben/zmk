6496b0f55b5a1d722fe1c5cacd606c08959ef24d
Oskari Seppa <oskari.seppa@haksi.fi>
2022-08-01 18:50:12 +0300

    Changed font, updated HDL

--- app/src/display.c
+++ app/src/display.c
@@ -18,36 +18,34 @@ unsigned char HDL_PAGE_OUTPUT[141] = {
 0x00, 0x00, 0x02, 0x04, 0x04, 0x01, 0x01, 0x05, 0x04, 0x01, 0x01, 0x02, 0x00, 0x00, 0x02, 0x07, 
 0x07, 0x01, 0x00, 0x0A, 0x04, 0x01, 0x02, 0x00, 0x00, 0x00, 0x02, 0x07, 0x07, 0x01, 0x01, 0x0A, 
 0x04, 0x01, 0x02, 0x00, 0x00, 0x00, 0x02, 0x04, 0x04, 0x01, 0x02, 0x05, 0x04, 0x01, 0x02, 0x01, 
-0x00, 0x31, 0x32, 0x0A, 0x33, 0x32, 0x00, 0x01, 0x0A, 0x04, 0x01, 0x03, 0x00
+0x00, 0x31, 0x32, 0x0A, 0x33, 0x32, 0x00, 0x01, 0x0A, 0x04, 0x01, 0x05, 0x00
 };
 
 LOG_MODULE_REGISTER(hdldisp, CONFIG_DISPLAY_LOG_LEVEL);
 
-void dsp_clear (uint16_t x, uint16_t y, uint16_t w, uint16_t h) {
+void dsp_clear (int16_t x, int16_t y, uint16_t w, uint16_t h) {
     il0323_clear_area(display, x, y, w, h);
-    LOG_ERR("Clear screen\n");
 }
 
-void dsp_render (uint16_t x, uint16_t y, uint16_t w, uint16_t h) {
-    LOG_ERR("Rendering screen %i %i %i %i", x, y, w, h);
+void dsp_render (int16_t x, int16_t y, uint16_t w, uint16_t h) {
     il0323_refresh(display, x, y, w, h);
 }
 
-void dsp_pixel (uint16_t x, uint16_t y) {
+void dsp_pixel (int16_t x, int16_t y) {
     il0323_set_pixel(display, x, y);
 }
 
-void dsp_hline (uint16_t x, uint16_t y, uint16_t len) {
+void dsp_hline (int16_t x, int16_t y, int16_t len) {
     il0323_h_line(display, x, y, len);
 }
 
-void dsp_vline (uint16_t x, uint16_t y, uint16_t len) {
+void dsp_vline (int16_t x, int16_t y, int16_t len) {
     il0323_v_line(display, x, y, len);
 }
 
-extern const char hdl_font[464];
+extern const char HDL_FONT[2048];
 
-void dsp_text (uint16_t x, uint16_t y, const char *text, uint8_t fontSize) {
+void dsp_text (int16_t x, int16_t y, const char *text, uint8_t fontSize) {
 
     int len = strlen(text);
     int line = 0;
@@ -55,7 +53,6 @@ void dsp_text (uint16_t x, uint16_t y, const char *text, uint8_t fontSize) {
 
     for (int g = 0; g < len; g++) {
 		// Starting character in single quotes
-		int off = (text[g] - '!') * 8;
 
         if (text[g] == '\n') {
 			line++;
@@ -65,17 +62,13 @@ void dsp_text (uint16_t x, uint16_t y, const char *text, uint8_t fontSize) {
 		else if (text[g] == ' ') {
 			acol++;
 		}
-
-		if (off < 0 && off > sizeof(hdl_font) / 8)
-			continue;
-
 		
 		for (int py = 0; py < 8; py++) {
-			for (int px = 0; px < 8; px++) {
-				if ((hdl_font[off + py] >> (7 - px)) & 1) {
-                    int rx = x + (px + acol * 10) * fontSize;
-                    int ry = y + (py + line * 10) * fontSize;
-                    
+			for (int px = 0; px < 5; px++) {
+				if ((HDL_FONT[text[g] * 8 + py] >> (7 - px)) & 1) {
+                    int rx = x + (px + acol * 5) * fontSize;
+                    int ry = y + (py + line * 6) * fontSize;
+
                     for(int sy = 0; sy < fontSize; sy++) {
                         for(int sx = 0; sx < fontSize; sx++) {
                             dsp_pixel(rx + sx, ry + sy);
@@ -94,9 +87,7 @@ int err = 0;
 static void display_thread(void *arg, void *unused2, void *unused3) {
     k_msleep(2000);
     // Initialize HDL
-    LOG_ERR("Creating interface");
     interface = HDL_CreateInterface(80, 128, HDL_COLORS_MONO, HDL_FEAT_TEXT | HDL_FEAT_LINE_HV);
-    LOG_ERR("Interface created");
 
     // Set interface functions
     interface.f_clear = dsp_clear;
@@ -105,17 +96,16 @@ static void display_thread(void *arg, void *unused2, void *unused3) {
     interface.f_hline = dsp_hline;
     interface.f_vline = dsp_vline;
     interface.f_text = dsp_text;
-    LOG_ERR("Building page");
+
+    interface.textHeight = 6;
+    interface.textWidth = 4;
 
     err |= HDL_Build(&interface, HDL_PAGE_OUTPUT, sizeof(HDL_PAGE_OUTPUT));
-
-    LOG_ERR("Page built");
 
     err |= HDL_Update(&interface);
 
     while(1) {
         k_msleep(60000);
-        LOG_ERR("ERR: %i\n", err);
         err |= HDL_Update(&interface);
 
         il0323_hibernate(display);
